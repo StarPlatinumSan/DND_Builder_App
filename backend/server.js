a472a1d325d4fec5490639fb9146c8f1dbf947aa
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2024-12-25 14:44:12 -0500

    api and backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
 const express = require("express");
 const cors = require("cors");
 
@@ -8,9 +13,25 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+	res.send("Backend is running");
+});
+
+app.get("/data", async (req, res) => {
+	try {
+		const { data, error } = await supabase.from("test").select("*");
+
+		if (error) {
+			console.error("Error fetching data: ", error.message);
+			return res.status(500).json({ error: error.message });
+		}
+
+		res.json(data);
+	} catch (err) {
+		console.error("Error: ", err.message);
+		return res.status(500).json({ error: "An unexpected error occurred" });
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port : ${port}`);
+	console.log(`Server is running on port : ${port}`);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,50 @@
 import { useEffect, useState } from "react";
+import axios from "./client/apiClient";
+
+interface Test {
+	id: number;
+	username: string;
+}
 
 function App() {
-  const [message, setMessage] = useState("");
+	const [message, setMessage] = useState("");
+	const [data, setData] = useState<Test[]>([]);
+	const [error, setError] = useState<string>("");
 
-  useEffect(() => {
-    fetch("http://localhost:3000")
-      .then((res) => res.text())
-      .then(setMessage);
-  }, []);
+	useEffect(() => {
+		fetch("http://localhost:3000")
+			.then((res) => res.text())
+			.then(setMessage);
+	}, []);
 
-  return (
-    <>
-      <main className="app">{message}</main>
-    </>
-  );
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/data")
+			.then((response: { data: Test[] }) => {
+				setData(response.data);
+			})
+			.catch((error: Error) => {
+				console.error("Erreur: ", error);
+				setError(error.message);
+			});
+	}, []);
+
+	return (
+		<>
+			<main className="app">
+				<h1>{message}</h1>
+				{error && <p>{error}</p>}
+
+				{data.length > 0 && (
+					<ul>
+						{data.map((item: Test) => (
+							<li key={item.id}>{item.username}</li>
+						))}
+					</ul>
+				)}
+			</main>
+		</>
+	);
 }
 
 export default App;
