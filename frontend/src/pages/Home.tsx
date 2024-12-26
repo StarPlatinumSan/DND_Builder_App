2959bd9fe0ec6ccf413e2c9a01c3b9c5c2a9cdea
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2024-12-26 12:58:51 -0500

    desktop UI

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -56,74 +56,104 @@ const Home = () => {
 			</section>
 
 			<section className="content">
-				<div className="category">
-					<h2>What is it?</h2>
-					<p className="description">Create characters, level them up, exploring other users' creations, and more!</p>
-					<div className="collapsible">
-						<button className="btnLearn btnSlide" onClick={toggleSlide}>
-							Click to find out
-						</button>
-						<SlideDown
-							style={{
-								transition: "height 0.2s ease-in-out",
-							}}
-						>
-							{open && (
-								<div className="slideDownContent">
-									<strong>An overhauled D&D character builder and controller</strong>
-									<ul>
-										<li>Create your characters from scratch with an easy to use and mostly automated interface!</li>
+				<div className="groupCategoryMain">
+					<div className="category mainCategory">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>What is it?</h2>
+						<p className="description">Create characters, level them up, exploring other users' creations, and more!</p>
+						<div className="collapsible">
+							<button className="btnLearn btnSlide" onClick={toggleSlide}>
+								Click to find out
+							</button>
+							<SlideDown
+								style={{
+									transition: "height 0.2s ease-in-out",
+								}}
+							>
+								{open && (
+									<div className="slideDownContent">
+										<strong>An overhauled D&D character builder and controller</strong>
+										<ul>
+											<li>Create your characters from scratch with an easy to use and mostly automated interface!</li>
 
-										<li>Save them on your profile and maintain them.</li>
-										<li>Level them up whenever needed.</li>
-										<li>Discover other people's public characters and save them on your profile.</li>
-										<li>Explore all classes, races, spells, feats and more!</li>
-									</ul>
-								</div>
-							)}
-						</SlideDown>
+											<li>Save them on your profile and maintain them.</li>
+											<li>Level them up whenever needed.</li>
+											<li>Discover other people's public characters and save them on your profile.</li>
+											<li>Explore all classes, races, spells, feats and more!</li>
+										</ul>
+									</div>
+								)}
+							</SlideDown>
+						</div>
+					</div>
+					<div className="category">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>What is Dungeons and Dragons?</h2>
+						<img src="dnd.jpg" alt="What is this app" className="imgCategory" />
+						<p className="description">
+							Dungeons & Dragons is a collaborative storytelling game that invites you to become a hero in a fantasy world of magic and monsters. Whether exploring ancient dungeons or forging alliances, the only limit is your imagination.
+						</p>
+						<button className="btnLearn">About</button>
 					</div>
 				</div>
 
-				<div className="category">
-					<h2>What is Dungeons and Dragons?</h2>
-					<img src="dnd.jpg" alt="What is this app" className="imgCategory" />
+				<div className="interCategory">
+					<h2>Why we built this app</h2>
 					<p className="description">
-						Dungeons & Dragons is a collaborative storytelling game that invites you to become a hero in a fantasy world of magic and monsters. Whether exploring ancient dungeons or forging alliances, the only limit is your imagination.
+						This non-official fanmade D&D app was created with the purpose of having an easy to use interface that automates most of the redundant stuff you need when creating a character. It focuses on what's important and auto-selects all the
+						non-modifiable content like the class features or abilities. It lets you have your characters always with you, so you can access them even without your character sheet. Moreover, you can level them up easily, making you save time with all the
+						search usually needed. It also serves as a database for all the spells, classes, races, features, etc.
 					</p>
-					<button className="btnLearn">About</button>
 				</div>
-
-				<div className="category">
-					<h2>Explore the spells</h2>
-					<img src="spells.jpg" alt="spells" className="imgCategory" />
-					<p className="description">From simple cantrips to legendary incantations, spells are the heart of any magic-wielding character. Unleash arcane energy, manipulate elements, and shape reality itself as you master the art of spellcasting.</p>
-					<button className="btnLearn">Explore</button>
-				</div>
-
-				<div className="category">
-					<h2>Dive into the classes</h2>
-					<img src="classes.webp" alt="spells" className="imgCategory" />
-					<p className="description">
-						Will you take up the sword as a mighty Fighter, delve into forbidden lore as a Warlock, or heal the party’s wounds as a Cleric? Each class offers distinct abilities and playstyles—choose the one that speaks to your heroic spirit.
-					</p>
-					<button className="btnLearn">Dive into</button>
-				</div>
-
-				<div className="category">
-					<h2>Discover the racial options</h2>
-					<img src="races.jpg" alt="spells" className="imgCategory" />
-					<p className="description">
-						From the stout Dwarves to the graceful Elves, every playable race brings its own history and culture to the table. Embrace unique traits, lore, and heritage that can shape the way your character views—and changes—the world.
-					</p>
-					<button className="btnLearn">Discover</button>
-				</div>
-
-				<div className="category">
-					<h2>Customize your character</h2>
-					<img src="character.jpg" alt="character" className="imgCategory" />
-					<p className="description">Build a hero as unique as you are! Refine your backstory, choose specialized skills, and select gear that fits your style. Your character’s journey is entirely in your hands—no two adventurers are ever the same.</p>
-					<button className="btnLearn">Start now!</button>
+				<div className="groupCategory">
+					<div className="category">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>What is Dungeons and Dragons?</h2>
+						<img src="dnd.jpg" alt="What is this app" className="imgCategory" />
+						<p className="description">
+							Dungeons & Dragons is a collaborative storytelling game that invites you to become a hero in a fantasy world of magic and monsters. Whether exploring ancient dungeons or forging alliances, the only limit is your imagination.
+						</p>
+						<button className="btnLearn">About</button>
+					</div>
+					<div className="category">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>Explore the spells</h2>
+						<img src="spells.jpg" alt="spells" className="imgCategory" />
+						<p className="description">From simple cantrips to legendary incantations, spells are the heart of any magic-wielding character. Unleash arcane energy, manipulate elements, and shape reality itself as you master the art of spellcasting.</p>
+						<button className="btnLearn">Explore</button>
+					</div>
+					<div className="category">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>Dive into your potential classes</h2>
+						<img src="classes.webp" alt="spells" className="imgCategory" />
+						<p className="description">
+							Will you take up the sword as a mighty Fighter, delve into forbidden lore as a Warlock, or heal the party’s wounds as a Cleric? Each class offers distinct abilities and playstyles, choose the one that speaks to your heroic spirit.
+						</p>
+						<button className="btnLearn">Dive into</button>
+					</div>
+					<div className="category">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>Discover the racial options</h2>
+						<img src="races.jpg" alt="spells" className="imgCategory" />
+						<p className="description">
+							From the stout Dwarves to the graceful Elves, every playable race brings its own history and culture to the table. Embrace unique traits, lore, and heritage that can shape the way your character views, and changes the world.
+						</p>
+						<button className="btnLearn">Discover</button>
+					</div>
+					<div className="category">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>Customize your character</h2>
+						<img src="character.jpg" alt="character" className="imgCategory" />
+						<p className="description">Build a hero as unique as you are! Refine your backstory, choose specialized skills, and select gear that fits your style. Your character’s journey is entirely in your hands, no two adventurers are ever the same.</p>
+						<button className="btnLearn">Start now!</button>
+					</div>
+					<div className="category" id="customCard">
+						<img src="/redpin.webp" alt="PIN" className="redPin" />
+						<h2>Share it with your friends</h2>
+						<img src="buildCharacter.jpg" alt="character" className="imgCategory" />
+						<p className="description">Save your characters on your profile, and decide which ones you want to set public for the others to see!</p>
+						<button className="btnLearn">How it works</button>
+					</div>
 				</div>
 			</section>
 
