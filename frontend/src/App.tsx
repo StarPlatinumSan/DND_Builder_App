6d464f956c7ae3532e9dec78e7ab9cb0b007b48f
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2025-01-20 11:14:55 -0500

    idk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import axios from "./client/apiClient";
 import { useEffect, useState } from "react";
+import axios from "./client/apiClient";
 import Home from "./pages/Home";
 import Vault from "./pages/Vault";
 import Login from "./components/Login";
@@ -10,46 +10,46 @@ import ClassSection from "./pages/Archive/ClassSection";
 import FeatSection from "./pages/Archive/FeatsSection";
 
 interface Test {
-	id: number;
-	username: string;
+  id: number;
+  username: string;
 }
 
 function App() {
-	const [message, setMessage] = useState("");
-	const [data, setData] = useState<Test[]>([]);
-	const [error, setError] = useState<string>("");
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState<Test[]>([]);
+  const [error, setError] = useState<string>("");
 
-	/* hook qui test le fetching d'une route */
-	useEffect(() => {
-		fetch("http://localhost:3000")
-			.then((res) => res.text())
-			.then(setMessage);
-	}, []);
+  /* hook qui test le fetching d'une route */
+  useEffect(() => {
+    fetch("http://localhost:3000")
+      .then((res) => res.text())
+      .then(setMessage);
+  }, []);
 
-	/* hook qui test le fetching d'une table de test de la supabase */
-	useEffect(() => {
-		axios
-			.get("http://localhost:3000/test")
-			.then((response: { data: Test[] }) => {
-				setData(response.data);
-			})
-			.catch((error: Error) => {
-				console.error("Error: ", error);
-				setError(error.message);
-			});
-	}, []);
+  /* hook qui test le fetching d'une table de test de la supabase */
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/test")
+      .then((response: { data: Test[] }) => {
+        setData(response.data);
+      })
+      .catch((error: Error) => {
+        console.error("Error: ", error);
+        setError(error.message);
+      });
+  }, []);
 
-	return (
-		<Routes>
-			<Route path="/" element={<Home />}></Route>
-			<Route path="/login" element={<Login />} />
-			<Route path="/classes" element={<ClassSection />} />
-			<Route path="/spells" element={<SpellSection />} />
-			<Route path="/feats" element={<FeatSection />}></Route>
-			<Route path="/vault" element={<Vault />}></Route>
-			<Route path="/characterCreation" element={<CharacterCreation />}></Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/classes" element={<ClassSection />} />
+      <Route path="/spells" element={<SpellSection />} />
+      <Route path="/feats" element={<FeatSection />}></Route>
+      <Route path="/vault" element={<Vault />}></Route>
+      <Route path="/characterCreation" element={<CharacterCreation />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
