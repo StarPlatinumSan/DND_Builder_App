a47528e73098d3f5024fb12d339dbfc0efc03812
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2025-01-23 15:58:25 -0500

    added homebrew slidedown

--- frontend/src/components/Feat.tsx
+++ frontend/src/components/Feat.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import SlidePlatinum from "./SlidePlatinum";
 
 interface Feat {
 	id: number;
@@ -9,34 +10,23 @@ interface Feat {
 }
 
 const FeatCard: React.FC<{ feat: Feat; addFeatToCharacter: (feat: Feat) => void }> = ({ feat, addFeatToCharacter }) => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	const toggleFeat = () => {
-		setIsOpen((prev) => !prev);
-	};
-
 	const handleSelect = (e: React.MouseEvent) => {
 		e.stopPropagation();
 		addFeatToCharacter(feat);
 	};
 
 	return (
-		<div className="featCard" onClick={toggleFeat}>
-			<h3>{feat.name}</h3>
-			<SlideDown>
-				{isOpen && (
-					<div className="featContent">
-						{feat.description.length > 0 && <p>{feat.description}</p>}
-						{feat.boni && feat.boni.length > 2 && <p>{feat.boni}</p>}
-						{feat.prerequisite && <span className="prerequisite">Prerequisite {feat.prerequisite || null}</span>}
+		<SlidePlatinum title={feat.name} duration={0.3} trigger="click" position="static" className="featCard">
+			<div className="featContent">
+				{feat.description.length > 0 && <p>{feat.description}</p>}
+				{feat.boni && feat.boni.length > 2 && <p>{feat.boni}</p>}
+				{feat.prerequisite && <span className="prerequisite">Prerequisite {feat.prerequisite || null}</span>}
 
-						<button className="btn maxWidth50" onClick={handleSelect}>
-							Select
-						</button>
-					</div>
-				)}
-			</SlideDown>
-		</div>
+				<button className="btn maxWidth50" onClick={handleSelect}>
+					Select
+				</button>
+			</div>
+		</SlidePlatinum>
 	);
 };
 
